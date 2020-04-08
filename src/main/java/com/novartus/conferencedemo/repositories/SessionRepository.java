0b9da1b2a8e8290be263b3902a3f1c67626360e5
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-09 03:06:18 +0530

    Junk Code

--- src/main/java/com/novartus/conferencedemo/repositories/SessionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.novartus.conferencedemo.repositories;
-
-import com.novartus.conferencedemo.models.Session;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SessionRepository extends JpaRepository<Session, Long> {
-
-}
--- src/main/java/com/novartus/conferencedemo/repositories/SpeakerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.novartus.conferencedemo.repositories;
-
-import com.novartus.conferencedemo.models.Speaker;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SpeakerRepository  extends  JpaRepository<Speaker, Long> {
-
-}
