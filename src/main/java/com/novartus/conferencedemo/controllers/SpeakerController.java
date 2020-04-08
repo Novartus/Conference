38d1c81b32fc45cec93748ccbc11e70e63fcfd37
Abhee Hudani <hudaniabhee@gmail.com>
2020-04-08 13:46:20 +0530

    CRUD Operations Added

--- src/main/java/com/novartus/conferencedemo/controllers/SpeakerController.java
+++ src/main/java/com/novartus/conferencedemo/controllers/SpeakerController.java
@@ -1,20 +1,18 @@
 package com.novartus.conferencedemo.controllers;
 
+import com.novartus.conferencedemo.models.Session;
 import com.novartus.conferencedemo.models.Speaker;
 import com.novartus.conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.security.PublicKey;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/speakers")
 public class SpeakerController {
-    @Autowired 
+    @Autowired
     private SpeakerRepository speakerRepository;
 
     @GetMapping
@@ -26,5 +24,24 @@ public class SpeakerController {
     @RequestMapping("{id}")
     public Speaker get(@PathVariable Long id){
         return speakerRepository.getOne(id);
+    }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker); //Flushes the data immediately during the execution
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        //Also need to check for children before deleting it.
+        speakerRepository.deleteById(id);
+    }
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        //Because this is a PUT, we expect all attributes to be pass in.
+        //TODO: Add validation that all attributes are passed in, otherwise return a 400 bad payload, if it has children.
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker,existingSpeaker,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
     }
 }
